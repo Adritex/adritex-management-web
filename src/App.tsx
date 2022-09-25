bc8bfbf00a105de8010a29fbfb2b213caf45f620
Gabriel Castellani <gabrielcastellanioliveira@gmail.com>
2022-09-25 15:32:43 -0300

    Create web project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "primereact/resources/themes/nova-accent/theme.css";
+import "primereact/resources/primereact.min.css";                 
+import "primeicons/primeicons.css";           
+
+function App() {
+  return (
+    <>
+      Root
+    </>
+  )
+}
+
+export default App
